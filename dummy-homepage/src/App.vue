2c02de86a448b0f77abf760763a464b0c12ae804
push23 <kolosov4dev@gmail.com>
2022-06-11 14:03:12 +0400

    Update App.vue

--- dummy-homepage/src/App.vue
+++ dummy-homepage/src/App.vue
@@ -6,7 +6,7 @@
         <div class="logo" style="float:left;"><a href="/"><img src="logo.svg" alt=""></a></div>
         </Col>
 
-        <Col xs={24} xl={8} style="width: 40%;">
+        <Col xs={24} xl={8} style="width: 30%;">
       <a-menu
         v-model:selectedKeys="selectedKeys"
         mode="horizontal"
