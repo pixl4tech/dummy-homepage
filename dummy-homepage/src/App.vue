1ef4a84ce14d774ae001d37fdefdb0365f98bf23
push23 <kolosov4dev@gmail.com>
2022-06-11 13:50:44 +0400

    Update App.vue
    fix layout for mobile

--- dummy-homepage/src/App.vue
+++ dummy-homepage/src/App.vue
@@ -1,11 +1,29 @@
 <template>
   <a-layout class="layout">
-    <a-layout-header :style="{ height: '80px', background: '#fff' }">
-      <div class="logo" style="float:left;"><a href="/"><img src="logo.svg" alt=""></a></div>
+    <a-layout-header :style="{ minHeight: '80px', background: '#fff' }">
+      <Row>
+        <Col xs={24} xl={8}>
+        <div class="logo" style="float:left;"><a href="/"><img src="logo.svg" alt=""></a></div>
+        </Col>
+
+        <Col xs={24} xl={8}>
+      <a-menu
+        v-model:selectedKeys="selectedKeys"
+        mode="horizontal"
+        :style="{color: '#000', lineHeight: '80px', fontWeight: 300}"
+      >
+        <a-menu-item key="1">Main page</a-menu-item>
+        <a-menu-item key="2">About</a-menu-item>
+        <a-menu-item key="3">Blog</a-menu-item>
+      </a-menu>
+      </Col>
+
+        <Col xs={24} xl={8} style="margin-left: auto; order: 2;">
        <div style="float:right; color:#000; margin-top: 8px;">
           <div style="float: left; margin-right: 15px;">
             <a-button type="dashed" ghost>Resume</a-button>
           </div>
+
           <div class="social" style="float: left; font-size: 24px;">
             <a href="https://www.linkedin.com/in/pixl4tech/"><linkedin-filled /></a>
             <a href="https://github.com/pixl4tech"><github-filled /></a>
@@ -16,27 +34,26 @@
             <a href="#"><instagram-filled /></a>
           </div>
         </div>
-      <a-menu
-        v-model:selectedKeys="selectedKeys"
-        mode="horizontal"
-        :style="{color: '#000', lineHeight: '80px', fontWeight: 300}"
-      >
-        <a-menu-item key="1">Main page</a-menu-item>
-        <a-menu-item key="2">About</a-menu-item>
-        <a-menu-item key="3">Blog</a-menu-item>
-      </a-menu>
-     
+        </Col>
+
+     </Row>
     </a-layout-header>
     <a-layout-content :style="{ background: '#45207E'}">
       <HelloWorld />
     </a-layout-content>
-    <a-layout-footer style="text-align: left; height: 60px; font-size:13px; padding: 20px 50px">
+    <a-layout-footer style="text-align: left; min-height: 60px; height:auto; font-size:13px; padding: 20px 50px">
+            <Row>
+        <Col xs={24} xl={12}>
       <p style="float: left">Kolosov.dev © 2022 Software Developer</p> 
+      </Col>
+        <Col xs={24} xl={12} style="margin-left: auto; order: 2;">
       <div class="footer-link" style="float: right">
         <a href="#">Contact email</a>
         <a href="#">Privacy</a>
         <a href="#">About</a>
       </div>
+      </Col>
+         </Row>
     </a-layout-footer>
   </a-layout>
 </template>
@@ -52,6 +69,7 @@ import {
         } from '@ant-design/icons-vue';
 import HelloWorld from './components/HelloWorld.vue'
 import { ref } from 'vue';
+import { Row, Col } from 'ant-design-vue';
 export default {
   name: 'App',
   components: {
@@ -62,7 +80,9 @@ export default {
     LinkedinFilled,
     MediumSquareFilled,
     BehanceCircleFilled,
-    TwitterCircleFilled
+    TwitterCircleFilled,
+    Row,
+    Col
   },
   setup() {
     return {
@@ -82,7 +102,7 @@ export default {
 
 .footer-link a {
   color: #7E7E7E;
-  margin-left: 15px;
+  margin-right: 15px;
 }
 
 .footer-link a:hover {
@@ -109,6 +129,10 @@ export default {
 .ant-row-rtl #components-layout-demo-top .logo {
   float: right;
   margin: 16px 0 16px 24px;
+}
+
+.ant-layout-header {
+    height: auto !important;
 }
 
 .ant-menu-item-selected, .ant-menu-item-active {
